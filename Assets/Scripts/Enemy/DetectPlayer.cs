88e6dce746fb7a7688f9bb73d736bd6a0ec4d09c
HarryBenn1 <harrisonbenn4@gmail.com>
2023-09-11 13:21:08 +1000

    Moving scences/scripts around

--- Assets/Scripts/Enemy/DetectPlayer.cs
+++ Assets/Scripts/Enemy/DetectPlayer.cs
@@ -1,80 +1,18 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour
+public class DetectPlayer : MonoBehaviour
 {
-    public float moveSpeed = 3f;       
-
-    private Transform cameraTransform;  
-    private bool isPlayerVisible = false;
-    private Camera mainCamera;
-    GameObject playerObject = GameObject.FindGameObjectWithTag("player");
-    public Transform player;            
-
-
+    // Start is called before the first frame update
     void Start()
     {
-        player = playerObject.transform;
-        // Find the main camera in the scene
-        Camera mainCamera = Camera.main;
-        if (mainCamera != null)
-        {
-            cameraTransform = mainCamera.transform;
-        }
-        else
-        {
-            Debug.LogError("Main Camera not found in the scene!");
-        }
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Check if the player is within the camera's view
-        if (IsPlayerVisible())
-        {
-            Console.WriteLine("Visible");
-            isPlayerVisible = true;
-        }
-        else
-        {
-             Console.WriteLine("Invisible");
-            isPlayerVisible = false;
-        }
-
-        // If the player is visible, move towards the player
-        if (isPlayerVisible)
-        {
-            MoveTowardsPlayer();
-        }
-    }
-
-    bool IsPlayerVisible()
-    {
-        if (cameraTransform == null || player == null)
-        {
-            return false;
-        }
-
-        // Calculate the viewport position of the player
-        Vector3 playerViewportPosition = mainCamera.WorldToViewportPoint(player.position);
-
-        // Check if the player is within the camera's viewport
-        if (playerViewportPosition.x >= 0f && playerViewportPosition.x <= 1f &&
-            playerViewportPosition.y >= 0f && playerViewportPosition.y <= 1f &&
-            playerViewportPosition.z > 0f)
-        {
-            return true;
-        }
-
-        return false;
-    }
-
-    void MoveTowardsPlayer()
-    {
-        // Calculate the direction to move towards the player
-        Vector3 direction = (player.position - transform.position).normalized;
-
-        // Move the enemy towards the player
-        transform.Translate(direction * moveSpeed * Time.deltaTime);
+        
     }
 }
