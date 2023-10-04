47a9447798c2fb01de6ec6d0f56034ad9d2d469f
Charles Pearse <Charles.J.Pearse@student.uts.edu.au>
2023-10-04 20:00:30 +1100

    Seperate Areas

--- Assets/Scripts/Portal/AreaPortal.cs
+++ Assets/Scripts/Portal/AreaPortal.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class AreaPortal : Portal
 {
     [SerializeField] private Vector2 teleportToPosition;
+    [SerializeField] public SectionsManager secManager;
 
     protected override void EnterPortal(PlayerMovement player)
     {
         base.EnterPortal(player);
         player.transform.position = teleportToPosition;
+
+        secManager.ActivateNewAreaEnemies();
     }
 }
