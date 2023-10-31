43dae2060565a3af03622782aec9b09193cd22cd
Amren Gill <amren.gill@gmail.com>
2023-10-31 15:52:47 +1100

    gave the short range grunt a shield

---
+++ Assets/Scripts/Enemy/ShieldedEnemyHealth.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldedEnemyHealth : EnemyHealth
+{
+    [SerializeField] private Animator shieldAnimator;
+    [SerializeField] private AnimationClip shieldBreakAnimation;
+    private bool shielded = true;
+    
+    public override bool TakeDamage(int amount, Attack attack)
+    {
+        if (!shielded)
+        {
+            return base.TakeDamage(amount, attack);
+        }
+        
+        if ((attack is MeleeAttack || attack is ChargedRangedAttack) && shielded)
+        {
+            shieldAnimator.SetTrigger("ShieldBreak");
+            StartCoroutine(DestroyShield());
+            shielded = false;
+            return true;
+        }
+
+        return true;
+    }
+
+    private IEnumerator DestroyShield()
+    {
+        yield return new WaitForSeconds(shieldBreakAnimation.length);
+        Destroy(shieldAnimator.gameObject);
+    }
+}
