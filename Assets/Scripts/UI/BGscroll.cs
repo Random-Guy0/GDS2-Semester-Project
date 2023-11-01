e2a011b586bddfba22ebce8080b645fe06d24852
Nyxikagi <71370198+Nyxikagi@users.noreply.github.com>
2023-11-02 03:42:33 +1100

    Babylonia Fixup + River Scrolling

--- Assets/Scripts/UI/BGscroll.cs
+++ Assets/Scripts/UI/BGscroll.cs
@@ -6,10 +6,15 @@ using UnityEngine.UI;
 public class BGscroll : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer bgImg;
-    [SerializeField] private float x, y;
+    [SerializeField] private float speedX = 0.2f;
+    [SerializeField] private float speedY = 0.0f;
 
     void Update()
     {
+        // X & Y
+        Vector2 offset = new Vector2(Time.deltaTime * speedX, 0);
+
+        bgImg.material.mainTextureOffset = offset;
         //bgImg.uvRect = new Rect(bgImg.uvRect.position + new Vector2(x, y) * Time.deltaTime, bgImg.uvRect.size);
     }
 }
